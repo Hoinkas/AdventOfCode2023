e62e5561e5b587517023fd934378abf827f7cca3
Hoinkas <26513286+Hoinkas@users.noreply.github.com>
2023-12-01 11:14:16 +0100

    Update FirstDayPuzzle.py

--- FirstDayPuzzle.py
+++ FirstDayPuzzle.py
@@ -18,7 +18,6 @@ for line in lines:
 print(firstTaskResult)
 
 #---------SECOND PUZZLE SOLUTION---------
-#Dictionary for every number name with corresponding numbers
 translator = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
 def replaceWordsWithNumbers(textLine):
