26c6ddd3e32c0093b7fb9e6a26e5c4d54a702ad3
Juli Kozuch <hoinkas@gmail.com>
2023-12-04 21:08:00 +0100

    Shortening code for four day solution

--- FourDay/FourDayPuzzle_Hoinkas.py
+++ FourDay/FourDayPuzzle_Hoinkas.py
@@ -8,16 +8,11 @@ def firstPuzzleSolution(lines):
   firstTaskResult = 0
 
   for line in lines:
-    twoCardsList = line.split(': ')[1].split('|')
-    twoCardsList = [cardList.strip() for cardList in twoCardsList]
-    
-    winningCardList = list(map(int, filter(None, twoCardsList[0].split(' '))))
-    guessingCardList = list(map(int, filter(None, twoCardsList[1].split(' '))))
+    twoCardsList = [cardList.strip() for cardList in line.split(': ')[1].split('|')]
+    [winningCardList, guessingCardList] = [map(int, filter(None, cardList.split(' '))) for cardList in twoCardsList]
 
     numberOfWinningCards = len(list(set(winningCardList).intersection(guessingCardList)))
-
-    if numberOfWinningCards > 0:
-      firstTaskResult += 2**(numberOfWinningCards-1)
+    if numberOfWinningCards > 0: firstTaskResult += 2**(numberOfWinningCards-1)
   
   return firstTaskResult
 
@@ -32,11 +27,10 @@ def secondPuzzleSolution(lines):
   for line in lines:
     gameAndCardList = line.split(': ')
     numberOfGame = int(re.findall(f"\d+", gameAndCardList[0])[0])
-    twoCardsList = [cardList.strip() for cardList in gameAndCardList[1].split('|')]
     gameDict[numberOfGame] = 1
-    
-    winningCardList = list(map(int, filter(None, twoCardsList[0].split(' '))))
-    guessingCardList = list(map(int, filter(None, twoCardsList[1].split(' '))))
+
+    twoCardsList = [cardList.strip() for cardList in gameAndCardList[1].split('|')]  
+    [winningCardList, guessingCardList] = [map(int, filter(None, cardList.split(' '))) for cardList in twoCardsList]
 
     numberOfWinningCards = len(list(set(winningCardList).intersection(guessingCardList)))
     gameWins[numberOfGame] = numberOfWinningCards
@@ -45,7 +39,6 @@ def secondPuzzleSolution(lines):
     for j in range(1, gameWins[i]+1):
       gameDict[i+j] += gameDict[i]
 
-  secondTaskResult = sum(gameDict.values())
-  return secondTaskResult
+  return sum(gameDict.values())
 
 print(secondPuzzleSolution(lines))
