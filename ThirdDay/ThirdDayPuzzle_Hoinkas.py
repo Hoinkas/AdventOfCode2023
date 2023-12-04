b57ca36a20a96e262e481039c862e8eb43ba2000
Juli Kozuch <hoinkas@gmail.com>
2023-12-04 19:00:22 +0100

    ThirdDay solution shorted

--- ThirdDay/ThirdDayPuzzle_Hoinkas.py
+++ ThirdDay/ThirdDayPuzzle_Hoinkas.py
@@ -1,15 +1,21 @@
 lines = []
-with open('AOC_3Day_Quest.txt', 'r', encoding='utf-8') as f:
+with open('AOC_3Day_Quest.txt') as f:
   l = f.readlines()
   lines = [entry.strip() for entry in l]
 
-# #---------FIRST PUZZLE SOLUTION---------
-import re, pandas as pd
+import re, pandas as pd, numpy
 
+def checkOverlaping(charIndex, number: re.Match):
+  if number.start() >= charIndex-3 and number.end() <= charIndex+3:
+    firstInterval = pd.Interval(charIndex-1, charIndex+1)
+    secondInterval = pd.Interval(number.start(), number.end(), closed='left')
+
+    return secondInterval.overlaps(firstInterval)
+
+# #---------FIRST PUZZLE SOLUTION---------
 def firstPuzzleSolution(lines):
   firstTaskResult = 0
   specialCharDictList = []
-  specialNumDictList = []
 
   for i in range(len(lines)):
     #List of special characters
@@ -22,39 +28,19 @@ def firstPuzzleSolution(lines):
     
     specialCharDictList.append(charsDict)
 
-    #List of numbers
-    specialNumsInRow = re.finditer(f"\d+", lines[i])
-    numDict = {}
-
-    for specialNum in specialNumsInRow:
-      if len(specialNum.group(0))> 0:
-        numDict[int(specialNum.start())] = int(specialNum.group(0))
-
-    specialNumDictList.append(numDict)
-
-  def checkOverlaping(indexToCheck, indexOfNumber, number):
-    firstInterval = pd.Interval(indexToCheck-1, indexToCheck+1, closed='both')
-    secondInterval = pd.Interval(indexOfNumber, indexOfNumber + len(str(number)) - 1, closed='both')
-    
-    return secondInterval.overlaps(firstInterval)
-
   for row in range(len(specialCharDictList)):
     for charIndex in specialCharDictList[row]:
       for j in range(-1,2):
-        if 0 <= row+j < len(specialNumDictList):
-          for key, number in specialNumDictList[row+j].items():
-            if checkOverlaping(charIndex, key, number):
-              firstTaskResult += number
+        if 0 <= row+j < len(specialCharDictList):
+          specialNumsInRow = re.finditer(f"\d+", lines[row+j])
+          firstTaskResult += sum([int(specialNum.group(0)) for specialNum in specialNumsInRow if(checkOverlaping(charIndex, specialNum))])
               
   return firstTaskResult
-print(firstPuzzleSolution(lines))
+# print(firstPuzzleSolution(lines))
 #---------Second PUZZLE SOLUTION---------
-import re, pandas as pd, numpy
-
 def secondPuzzleSolution(lines):
   secondTaskResult = 0
   specialCharDictList = []
-  specialNumDictList = []
 
   for i in range(len(lines)):
     #List of special characters
@@ -67,30 +53,16 @@ def secondPuzzleSolution(lines):
     
     specialCharDictList.append(charsDict)
 
-    #List of numbers
-    specialNumsInRow = re.finditer(f"\d+", lines[i])
-    numDict = {}
-
-    for specialNum in specialNumsInRow:
-      if len(specialNum.group(0))> 0:
-        numDict[int(specialNum.start())] = int(specialNum.group(0))
-
-    specialNumDictList.append(numDict)
-
-  def checkOverlaping(indexToCheck, indexOfNumber, number):
-    firstInterval = pd.Interval(indexToCheck-1, indexToCheck+1, closed='both')
-    secondInterval = pd.Interval(indexOfNumber, indexOfNumber + len(str(number)) - 1, closed='both')
-    
-    return secondInterval.overlaps(firstInterval)
-
   for row in range(len(specialCharDictList)):
     for charIndex in specialCharDictList[row]:
       numbersToMultiply = []
       for j in range(-1,2):
-        if 0 <= row+j < len(specialNumDictList):
-          for key, number in specialNumDictList[row+j].items():
-            if checkOverlaping(charIndex, key, number):
-              numbersToMultiply.append(number)
+        if 0 <= row+j < len(specialCharDictList):
+          specialNumsInRow = re.finditer(f"\d+", lines[row+j])
+
+          for specialNum in specialNumsInRow:
+            if(checkOverlaping(charIndex, specialNum)):
+              numbersToMultiply.append(int(specialNum.group(0)))
 
       if(len(numbersToMultiply)>1):
         secondTaskResult += numpy.prod(numbersToMultiply)
