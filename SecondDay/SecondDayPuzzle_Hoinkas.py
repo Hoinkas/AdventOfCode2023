1df8d745527ee8193181c8fdb427a9b2ed96450a
Juli Kozuch <hoinkas@gmail.com>
2023-12-05 17:34:43 +0100

    Shortened second day code

--- SecondDay/SecondDayPuzzle_Hoinkas.py
+++ SecondDay/SecondDayPuzzle_Hoinkas.py
@@ -9,27 +9,27 @@ rules = {"red": 12, "green": 13, "blue": 14}
 firstTaskResult = 0
 
 for line in lines:
-  colorsAndValues = re.finditer(f"(\d+) ({'|'.join(rules.keys())})", line)
-  isGameValid = all(int(draw.group(1)) <= rules[draw.group(2)] for draw in colorsAndValues)
+  matches = re.finditer(r"(\d+) ({'|'.join(rules.keys())})", line)
+  isGameValid = all(int(match.group(1)) <= rules[match.group(2)] for match in matches)
 
   if isGameValid:
-    numberOfGame = int(re.search(f"Game (\d+)", line).group(1))
+    numberOfGame = int(re.search(r"Game (\d+)", line).group(1))
     firstTaskResult += numberOfGame
 
 print(firstTaskResult)
 
 #---------SECOND PUZZLE SOLUTION---------
-import numpy
+import numpy as np
 secondTaskResult = 0
 
 for line in lines:
   gameMinValuesDict = {"red": 0, "green": 0, "blue": 0}
 
   for color in gameMinValuesDict.keys():
-    valuesOfColor = re.finditer(f"(\d+) {color}", line)
+    valuesOfColor = re.finditer(r"(\d+) {color}", line)
     gameMinValuesDict[color] = max(int(value.group(1)) for value in valuesOfColor)
   
-  multipliedMaxValues = numpy.prod(list(gameMinValuesDict.values()))
+  multipliedMaxValues = np.prod(list(gameMinValuesDict.values()))
   secondTaskResult += multipliedMaxValues
 
 print(secondTaskResult)
