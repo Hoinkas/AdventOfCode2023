391bb4acaf603d70c13c775c96a9e55f1f70b0f4
Juli Kozuch <j.kozuch@optimumpareto.com>
2023-12-07 00:04:03 +0100

    Optimised first solution for day 6'th

--- SixthDay/SixthDayPuzzle_Hoinkas.py
+++ SixthDay/SixthDayPuzzle_Hoinkas.py
@@ -4,7 +4,7 @@ with open('AOC_6Day_Quest.txt') as f:
   lines = [entry.strip() for entry in l]
 
 #---------FIRST PUZZLE SOLUTION---------
-import re, numpy
+import re, numpy, math
 
 numbersList = [re.findall(r"(\d+)", line) for line in lines]
 timesList = list(map(int, [time for time in numbersList[0]]))
@@ -13,13 +13,13 @@ recordList = list(map(int, [time for time in numbersList[1]]))
 racesWinsList = []
 
 for raceNumber in range(len(timesList)):
-  winsCount = 0
-  for pressTime in range(timesList[raceNumber]):
-    timeLeft = timesList[raceNumber] - pressTime
-    reachedDistance = timeLeft * pressTime
+  timeOfRace = timesList[raceNumber]
 
-    if reachedDistance > recordList[raceNumber]: winsCount += 1
+  delta = timeOfRace**2 - (4 * recordList[raceNumber])
+  firstWinInput = math.ceil(((-timeOfRace) + math.sqrt(delta))/2*(-1))
+  secondWinInput = math.floor(((-timeOfRace) - math.sqrt(delta))/2*(-1))
 
+  winsCount = secondWinInput - firstWinInput + 1
   racesWinsList.append(winsCount)
 
 firstTaskResult = numpy.prod(racesWinsList)
