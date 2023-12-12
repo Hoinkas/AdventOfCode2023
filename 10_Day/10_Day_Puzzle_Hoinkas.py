536a9ba3a49495ea6430a588aad08cbae05a12d6
Juli Kozuch <hoinkas@gmail.com>
2023-12-12 18:50:16 +0100

    Solved second task for 10'th day

--- 10_Day/10_Day_Puzzle_Hoinkas.py
+++ 10_Day/10_Day_Puzzle_Hoinkas.py
@@ -3,11 +3,28 @@ with open('AOC_10_Day_Quest.txt') as f:
   l = f.readlines()
   matrix = [entry.strip() for entry in l]
 
-#---------FIRST PUZZLE SOLUTION---------
-import re
+#---------FUNCTIONS---------
+def returnPoints(matrix, pipesDict, neighborsPoints):
+  import re
 
-startingPosition = next(((i, match.start()) for i, row in enumerate(matrix) if (match := re.search(r"S", row))), (0, 0))
+  startingPosition = next(((i, match.start()) for i, row in enumerate(matrix) if (match := re.search(r"S", row))), (0, 0))
 
+  points = []
+  sY = startingPosition[0]
+  sX = startingPosition[1]
+
+  for point in neighborsPoints:
+    i, j = point
+
+    if(0 <= sY+i < len(matrix) and 0 <= sX+j < len(matrix[0]) and matrix[sY+i][sX+j] != '.'):
+      currentSymbol = matrix[sY+i][sX+j]
+      pipesNextY, pipesNextX = [pipesDict[currentSymbol][i][iterat+1] for i, iterat in enumerate(point)]
+
+      if(pipesNextY != '' and pipesNextX != ''): points.append((sY+i,sX+j))
+  
+  return points, sY, sX
+
+#---------DEFAULT VARIABLES---------
 #Rules for each type of pipe telling where the next pipe is
 pipesDict = {
   '-': [['',0,''],[2,'',-2]],
@@ -18,40 +35,72 @@ pipesDict = {
   'F': [['',1,-1],['',1,-1]]
 }
 
-routes = []
-sY = startingPosition[0]
-sX = startingPosition[1]
-
 #Get all pipes connected to starting point
 neighborsPoints = [[-1,0],[0,-1],[0,1],[1,0]]
 
-for point in neighborsPoints:
-  i, j = point
+#---------FIRST PUZZLE SOLUTION---------
+def firstTaskSolution(matrix):
+  points, sY, sX = returnPoints(matrix, pipesDict, neighborsPoints)
 
-  if(0 <= sY+i < len(matrix) and 0 <= sX+j < len(matrix[0]) and matrix[sY+i][sX+j] != '.'):
-    currentSymbol = matrix[sY+i][sX+j]
-    pipesNextY, pipesNextX = [pipesDict[currentSymbol][i][iterat+1] for i, iterat in enumerate(point)]
+  furthestPoint = 1
+  previousPoints = [(sY,sX), (sY,sX)]
 
-    if(pipesNextY != '' and pipesNextX != ''): routes.append((sY+i,sX+j))
+  while points[0] != points[1]:
+    furthestPoint += 1
 
-furthestPoint = 1
-previousPoints = [(sY,sX), (sY,sX)]
+    for i, route in enumerate(points):
+      pointsDifference = (previousPoints[i][0] - route[0], previousPoints[i][1] - route[1])
+      
+      currentSymbol = matrix[route[0]][route[1]]
+      pipeCalc = pipesDict[currentSymbol]
+      prevPoint = previousPoints[i]
 
-while routes[0] != routes[1]:
-  furthestPoint += 1
+      pipesNextY, pipesNextX = [pipeCalc[i][diff+1] for i, diff in enumerate(pointsDifference)]
+      nextCoordY, nextCoordX = (prevPoint[0] + pipesNextY, prevPoint[1] + pipesNextX)
 
-  for i, route in enumerate(routes):
-    pointsDifference = (previousPoints[i][0] - route[0], previousPoints[i][1] - route[1])
+      points[i] = (nextCoordY, nextCoordX)
+
+      previousPoints[i] = route
+
+  return furthestPoint
+
+print(firstTaskSolution(matrix))
+
+def secondTaskSolution(matrix):
+  points, sY, sX = returnPoints(matrix, pipesDict, neighborsPoints)
+  
+  points = [(sY, sX), points[0]]
+
+  while True:
+    pointsDifference = (points[-2][0] - points[-1][0], points[-2][1] - points[-1][1])
     
-    currentSymbol = matrix[route[0]][route[1]]
+    currentSymbol = matrix[points[-1][0]][points[-1][1]]
     pipeCalc = pipesDict[currentSymbol]
-    prevPoint = previousPoints[i]
 
     pipesNextY, pipesNextX = [pipeCalc[i][diff+1] for i, diff in enumerate(pointsDifference)]
-    nextCoordY, nextCoordX = (prevPoint[0] + pipesNextY, prevPoint[1] + pipesNextX)
+    
+    nextCoordY, nextCoordX = (points[-2][0] + pipesNextY, points[-2][1] + pipesNextX)
+    newPoint = (nextCoordY, nextCoordX)
 
-    routes[i] = (nextCoordY, nextCoordX)
+    if(newPoint == (sY,sX)): break
+    points.append(newPoint)
+  
+  # Shoelace formula
+  # A = 1/2 * sum(xi * yi+1 - yi * xi+1)
 
-    previousPoints[i] = route
+  # Pick's theorem
+  # A = i + points/2 - 1    i - interior points
+  # i = A - points/2 + 1
 
-print(furthestPoint)
+  sumOfPoints = 0
+  howManyPoints = len(points)
+
+  for i in range(howManyPoints):
+    nextIndex = (i+1) % howManyPoints
+    sumOfPoints += points[i][1] * points[nextIndex][0] - points[i][0] * points[nextIndex][1]
+
+  result = round((sumOfPoints/2) - howManyPoints/2 + 1)
+
+  return result
+
+print(secondTaskSolution(matrix))
