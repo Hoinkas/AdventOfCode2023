c0280006f57428ad51f9c8d022a8bd51bcf90fe1
Juli Kożuch <j.kozuch@optimumpareto.com>
2024-10-17 18:57:11 +0200

    Changed names

--- FirstDay/No1PuzzleZbigniew.py
+++
@@ -1,5 +0,0 @@
-# AOC first puzzle
-No1PuzzleInput = ''
-No1PuzzleInput.read('No1PuzzleZbigniewInput.txt')
-#fsdfdsfsff
-#
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AdventOfCode2023
+
+Solution for https://adventofcode.com/2023 challenges
